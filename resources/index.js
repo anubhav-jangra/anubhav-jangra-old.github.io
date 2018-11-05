50e5c0a84af49c5856c02bb60fddeab8508c9d86
love-san <anubhav0603@gmail.com>
2018-11-06 04:22:05 +0530

    make some style changes

--- resources/index.js
+++ resources/index.js
@@ -1,6 +1,31 @@
-var info = document.querySelector(".info");
-var navbar = document.getElementById('navbar');
+var info = $(".info");
+var navbar = $('.navbar');
 var sticky = navbar.scrollHeight;
-console.log(sticky);
-console.log(info);
-info.style.marginTop = sticky + 'px';
+
+$('.home-link').on('click', function() {
+	$('#home').collapse('show');
+	$('#research').collapse('hide');
+	$('#proj').collapse('hide');
+	$('#cont').collapse('hide');
+});
+
+$('.research-link').on('click', function() {
+	$('#home').collapse('hide');
+	$('#research').collapse('show');
+	$('#proj').collapse('hide');
+	$('#cont').collapse('hide');
+});
+
+$('.project-link').on('click', function() {
+	$('#home').collapse('hide');
+	$('#research').collapse('hide');
+	$('#proj').collapse('show');
+	$('#cont').collapse('hide');
+});
+$('.contact-link').on('click', function() {
+	$('#home').collapse('hide');
+	$('#research').collapse('hide');
+	$('#proj').collapse('hide');
+	$('#cont').collapse('show');
+});
+
